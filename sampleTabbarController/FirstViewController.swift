47b0861af323f259f7369ad7b5b0b675be72b035
Riho Okubo <rihoohkubo1002@gmail.com>
2016-05-05 13:41:39 +0800

    Initial Commit

---
+++ sampleTabbarController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sampleTabbarController
+//
+//  Created by RIho OKubo on 2016/05/05.
+//  Copyright © 2016年 RIho OKubo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
